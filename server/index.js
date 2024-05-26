42473225dd09ee3e7a019c8aa7d5ae4edc572387
Vidit Prajapati <vidit.msit@gmail.com>
2024-05-26 14:29:09 +0530

    gitignore changes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log('DB Connection successful ✌️')
+})
+.catch((err)=>{
+    console.log(err)
+})
 
 const app = express() ;
 
